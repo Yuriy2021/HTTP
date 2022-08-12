ad735eb5cd0fb84e812763f738bfacf93fde7bfd
Yuriy <xstream_06@mail.ru>
2022-08-12 16:24:08 +0300

    HW_2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const requestListener = (req, res) => {
             res.writeHead(200);
             res.end('success');
         } else if (req.url === '/redirect' && req.method === 'GET') {
-            res.writeHead(200);
+            res.writeHead(301);
             res.end("Server is available on HTTP//localhost/redirected");
         };
         
