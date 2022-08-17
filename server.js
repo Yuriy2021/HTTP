90d52c6e1a3ea06af0bd2cecd56a7369624ee946
Yuriy <xstream_06@mail.ru>
2022-08-17 16:12:40 +0300

    HW2_update

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const fs = require ('fs');
 const files= [];
 
 const requestListener = (req, res) => {
+    const user = {
+        id:123,
+        username:'testuser',
+        password:'qwerty'
+    };
     
     if (req.url === '/get'&& req.method === 'GET') {
                 res.writeHead(200);
@@ -25,7 +30,18 @@ const requestListener = (req, res) => {
             res.setHeader('Location','/redirected');
             res.writeHead(301);
             res.end("Server is available on HTTP//localhost/redirected");
-        };
+        }else if (req.url === '/auth'&& req.method === 'POST') {
+            const client = String(req.username);
+            const pass = String(req.password);
+
+            if ( user.username === client && user.password === pass) {
+                res.writeHead(200);
+                res.end('auth success');
+            }else
+            res.writeHead(404);
+            res.end("auth failed");
+;
+        }
         
         res.writeHead(405);
         res.end("Method is not allowed");
